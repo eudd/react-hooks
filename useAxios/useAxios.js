961795d1d9fd87c5aa442eca9795d354048d2e25
kim.eudd <kim.eudd@gsnict.com>
2023-04-12 11:38:52 +0900

    first commit

---
+++ useAxios/useAxios.js
@@ -0,0 +1,30 @@
+import Axios from "axios";
+import { useEffect, useState } from "react";
+
+export const useAxios = (options, axiosInstances = Axios) => {
+  const [state, setState] = useState({
+    loading: true,
+    data: null,
+    error: null
+  });
+  const [trigger, setTrigger] = useState(0);
+  useEffect(() => {
+    if (!options?.url) {
+      console.error("no url");
+      return;
+    }
+    axiosInstances(options)
+      .then((res) => {
+        setState({ loading: false, data: res, error: null });
+      })
+      .catch((error) => {
+        setState({ loading: false, error, data: null });
+        console.error(error);
+      });
+  }, [trigger]);
+  const refetch = () => {
+    setState({ loading: true, data: null, error: null });
+    setTrigger(Date.now());
+  };
+  return { ...state, refetch };
+};
