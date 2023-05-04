181f18d3b9b2e83a2ce9dff263c844cf51fdea5d
kimeudd@gmail.com <kimeudd@gmail.com>
2023-05-05 01:51:48 +0900

    add refetch argument in "useAxios"

--- useAxios/useAxios.js
+++ useAxios/useAxios.js
@@ -2,27 +2,30 @@ import Axios from "axios";
 import { useEffect, useState } from "react";
 
 const useAxios = (options, axiosInstances = Axios) => {
-  const [state, setState] = useState({
-    loading: true,
-    data: null,
-    error: null
-  });
+  const [option, setOption] = useState(options);
+  const [state, setState] = useState({ loading: true, data: null, error: null });
   const [trigger, setTrigger] = useState(0);
   useEffect(() => {
-    if (!options?.url) {
-      console.error("no url");
-      return;
+    if (!option?.url) {
+      throw new Error("'url' must be in axios options");
     }
-    axiosInstances(options)
-      .then((res) => {
+    const call = async()=>{
+      try {
+        const res = await axiosInstances.request(option);
         setState({ loading: false, data: res, error: null });
-      })
-      .catch((error) => {
+      } catch (error) {
         setState({ loading: false, error, data: null });
-        console.error(error);
-      });
+      }
+    }
+    call();
   }, [trigger]);
-  const refetch = () => {
+  const refetch = (options) => {
+    if(options){
+      if(typeof(options)!=='object'){
+        throw new Error("options has to be json type");
+      }
+      setOption(options)
+    }
     setState({ loading: true, data: null, error: null });
     setTrigger(Date.now());
   };
